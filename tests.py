1290b67c2f72f07aabcb66e41986eef36cc4d996
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2023-01-23 20:32:01 +0300

    Update: test.py

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 #!./venv/bin/python
 
-import migrate
+from migrate import migrate
 from db.models.Movie import Movie
 from db.models.MovieEntry import MovieEntry
 from db.models.MovieTag import MovieTag
@@ -8,28 +8,39 @@ from db.models.MovieTag import MovieTag
 
 def run_tests():
 
-    # migrate.migrate(drop=True)
+    # migrate(drop=True)
 
-    print(Movie.getTable())
-    print(MovieEntry.getTable())
-    print(MovieTag.getTable())
+    print('--------------------------------------------------')
+    print('Auto table detection')
+    print()
+    print('Movie: ' + Movie.getTable())
+    print('Movie: ' + MovieEntry.getTable())
+    print('Movie: ' + MovieTag.getTable())
 
+    print('--------------------------------------------------')
+    print('Create DB query')
+    print()
     Movie.create({'title': 'First movie', 'rate': 4.7})
     Movie.create({'title': 'second movie', 'rate': 8.9})
 
-    print("--------------------------------------------------")
-    print(f"Columns: {Movie.columns()}")
-    print(f"Length of all movies: {len(Movie.all())}")
+    print('--------------------------------------------------')
+    print('DB Info')
+    print()
+    print(f'Columns: {Movie.columns()}')
+    print(f'Total Movie count: {len(Movie.all())}')
 
+    print('--------------------------------------------------')
+    print('DB Query')
+    print()
     id = 1
     movie = Movie.find(id)
     if movie is not None:
-        print(f"Found model id: {movie['id']}")
+        print(f"Find model with [id = {id}]: {movie['title']}")
     else:
-        print(f"Model with id {id} not found")
-
+        print(f'Find model with [id = {id}]: [Nothing]')
     like_cond = '%First%'
-    print(f"{like_cond}: {len(Movie.where(['title', 'LIKE', like_cond]))}")
+    like_cond_model_count = len(Movie.where(['title', 'LIKE', like_cond]))
+    print(f"Count of model [title LIKE {like_cond}]: {like_cond_model_count}")
 
 
 if __name__ == '__main__':
